8ba512e22e690d131552eeb3f07b4b1f4dea2b6e
thiagoyellow <thiago.g.santos@outlook.com.br>
2022-01-28 14:44:51 -0300

    edit: comment test

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React whith RocketSeat
         </a>
       </header>
     </div>
